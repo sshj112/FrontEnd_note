e8dc3fb6a6f92299f1db64891ff5aa56958dd68a
sshj <songhyunji1102@gmail.com>
2021-07-23 23:33:13 +0900

    2021.07.23 CloneCoding_Kakao 4th commit

--- CloneCoding/Kakao/date.js
+++ CloneCoding/Kakao/date.js
@@ -1,1 +1,35 @@
-// 날짜 요일 정보 Date()
+const dateContent = document.querySelector(".second_line");
+
+function getDayKor(day) {
+    if (day === 0) {
+        return "일";
+    } else if (day == 1){
+        return "월";
+    } else if (day == 2){
+        return "화";
+    } else if (day == 3){
+        return "수";
+    } else if (day == 4){
+        return "목";
+    } else if (day == 5){
+        return "금";
+    } else if (day == 6){
+        return "토";
+    } else {
+        return "wrong";
+    }
+}
+
+function getToday() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+    const day = today.getDay();
+    dateContent.innerText = `${month}월 ${date}일 ${getDayKor(day)}요일 소식입니다`;
+}
+
+function init() {
+    getToday();
+}
+
+init();
